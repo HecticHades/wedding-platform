eb14ee9c7bce441b07c406392e43efa08381ac6d
HecticHades <erich.mr@gmail.com>
2026-01-17 18:29:58 +0100

    fix(06-04): add unstructuredReference to sepa type declaration

--- src/types/sepa-payment-qr-code.d.ts
+++ src/types/sepa-payment-qr-code.d.ts
@@ -6,6 +6,7 @@ declare module "sepa-payment-qr-code" {
     amount?: number;
     currency?: string;
     remittanceInfo?: string;
+    unstructuredReference?: string;
     information?: string;
   }
 
